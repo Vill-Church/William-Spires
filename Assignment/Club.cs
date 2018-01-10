ff43c42eb64aa9c86fd615c5886911f7c7a89ae1
spiresw <spiresw@CUCWSH3A7872>
2018-01-10 17:44:35 +0000

    d

---
+++ Assignment/Club.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Class1
+{
+    private List<string> Employees = new List<string>();
+	public Class1()
+	{
+
+	}
+    public void ReadCSV()
+    {
+        Stream filePath = null;
+        OpenFileDialog openFile = new OpenFileDialog();
+        openFile.Title = "Select CSV File";
+        openFile.Filter = "CSV File|*.csv";
+        openFile.Show();
+
+    }
+}
